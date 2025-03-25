8334ef5b830d97d167e7107824aae2c62541a911
Katakarn Supserm <katakarn@hotmail.com>
2025-03-26 00:00:02 +0700

    🛠️ fix vercel error

--- src/pages/Example2.tsx
+++ src/pages/Example2.tsx
@@ -1,9 +1,9 @@
 import Card from "../components/Card"
-import { menu } from "../util/Menu"
+import { CardProps, menu } from "../util/menu"
 const Example2 = () => {
     return (
         <div className="flex flex-wrap gap-8 justify-center">
-            {menu.map((item, index) => (
+            {menu.map((item: CardProps, index: number) => (
                 <Card key={index} title={item.title} price={item.price} picture={item.picture} point={item.point} star={item.star} />
             ))}
         </div>
